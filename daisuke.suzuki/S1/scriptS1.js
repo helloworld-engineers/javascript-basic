781aec482cb968ef57a521a101c6ff19df5699d5
DaisukeSuzuki <daisuke.suzuki@helloworld.works>
2026-06-10 11:26:16 +0900

    feat:応用課題1の作成

---
+++ daisuke.suzuki/S1/scriptS1.js
@@ -0,0 +1,49 @@
+let rnd = 0;
+let scr1 = 0;
+let scr2 = 0;
+
+const diceNum1 = document.getElementById("dice1pc");
+//console.log(diceNum1);
+const diceNum2 = document.getElementById("dice2pc");
+const battleBtn = document.getElementById("battleBtn");
+const maxRound = 3;
+const result = document.getElementById("result");
+const plr1Score = document.getElementById("score1");
+const plr2Score = document.getElementById("score2");
+const roundCount = document.getElementById("round");
+
+//サイコロを振って出た値を返す
+function rollDice() {
+  const num = Math.floor(Math.random() * 6) + 1;
+  return num;
+}
+
+//ボタンクリックでサイコロを振る
+battleBtn.addEventListener("click", () => {
+  if (rnd <= maxRound) {
+    const num1 = rollDice();
+    const num2 = rollDice();
+    diceNum1.src = num1 + ".jpg";
+    diceNum2.src = num2 + ".jpg";
+    if (num1 > num2) {
+      scr1++;
+    } else if (num1 < num2) {
+      scr2++;
+    }
+    rnd++;
+    plr1Score.textContent = `プレイヤー1 : ${scr1} ポイント`;
+    plr2Score.textContent = `プレイヤー2 : ${scr2} ポイント`;
+    roundCount.textContent = `ラウンド数  : ${rnd} ラウンド`;
+  }
+  if (maxRound === rnd) {
+    document.getElementById("battleBtn").disabled = true;
+    if (scr1 > scr2) {
+      result.textContent = "プレイヤー1WIN";
+    } else if (scr1 < scr2) {
+      result.textContent = "プレイヤー2WIN";
+    } else {
+      result.textContent = "DRAW...";
+    }
+    return;
+  }
+});
