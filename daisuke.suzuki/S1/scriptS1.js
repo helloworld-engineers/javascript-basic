f08dc6aa02dbc506e3800e743660b822aa8693c1
DaisukeSuzuki <daisuke.suzuki@helloworld.works>
2026-06-10 13:35:57 +0900

    feat:指摘点修正

--- daisuke.suzuki/S1/scriptS1.js
+++ daisuke.suzuki/S1/scriptS1.js
@@ -1,15 +1,15 @@
 let round = 0;
-let scr1 = 0;
-let scr2 = 0;
+let playerScore1 = 0;
+let playerScore2 = 0;
 
-const diceNum1 = document.getElementById("dice1pc");
-const diceNum2 = document.getElementById("dice2pc");
-const battleBtn = document.getElementById("battleBtn");
 const maxRound = 3;
-const result = document.getElementById("result");
+const roundCount = document.getElementById("round");
 const plr1Score = document.getElementById("score1");
 const plr2Score = document.getElementById("score2");
-const roundCount = document.getElementById("round");
+const battleBtn = document.getElementById("battleBtn");
+const result = document.getElementById("result");
+const diceNum1 = document.getElementById("dice1pc");
+const diceNum2 = document.getElementById("dice2pc");
 
 //サイコロを振って出た値を返す
 function rollDice() {
@@ -17,31 +17,47 @@ function rollDice() {
   return num;
 }
 
+//勝敗を判定する
+function determineWinner() {
+  if (playerScore1 > playerScore2) {
+    result.textContent = "プレイヤー1WIN";
+  } else if (playerScore1 < playerScore2) {
+    result.textContent = "プレイヤー2WIN";
+  } else {
+    result.textContent = "DRAW...";
+  }
+}
+
+//スコアリスト表示する
+function scoreList() {
+  plr1Score.textContent = `プレイヤー1 : ${playerScore1} ポイント`;
+  plr2Score.textContent = `プレイヤー2 : ${playerScore2} ポイント`;
+  ``;
+  roundCount.textContent = `ラウンド数 : ${round} ラウンド`;
+}
+
+//ダイス画像表示する
+function dicePic(diceNum, number) {
+  diceNum.src = `${number}` + ".jpg";
+}
+
 //ボタンクリックでサイコロを振る
 battleBtn.addEventListener("click", () => {
   if (round < maxRound) {
     const num1 = rollDice();
     const num2 = rollDice();
-    diceNum1.src = num1 + ".jpg";
-    diceNum2.src = num2 + ".jpg";
+    dicePic(diceNum1, num1);
+    dicePic(diceNum2, num2);
     if (num1 > num2) {
-      scr1++;
+      playerScore1++;
     } else if (num1 < num2) {
-      scr2++;
+      playerScore2++;
     }
     round++;
-    plr1Score.textContent = `プレイヤー1 : ${scr1} ポイント`;
-    plr2Score.textContent = `プレイヤー2 : ${scr2} ポイント`;
-    roundCount.textContent = `ラウンド数 : ${round} ラウンド`;
+    scoreList();
   }
   if (maxRound === round) {
     document.getElementById("battleBtn").disabled = true;
-    if (scr1 > scr2) {
-      result.textContent = "プレイヤー1WIN";
-    } else if (scr1 < scr2) {
-      result.textContent = "プレイヤー2WIN";
-    } else {
-      result.textContent = "DRAW...";
-    }
+    determineWinner();
   }
 });
