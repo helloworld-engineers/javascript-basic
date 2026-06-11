8f542db3e9a337988345b7ba02fb4028474900b3
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-06-11 14:30:41 +0900

    イメージファイル名の説明、初期値のずれを修正、UI上でボタンを無効化しました

--- syuto-yasuda/jsouyoukadai1/script.js
+++ syuto-yasuda/jsouyoukadai1/script.js
@@ -1,4 +1,5 @@
 // プレイヤー名でサイコロの目のイラストが変わる関数
+// 指定されているイメージファイルはそれぞれサイコロの目のイメージ図になります。
 const playerScore = (playerNumber) => {
   const changeImage = document.getElementById(playerNumber);
   const score = Math.floor(Math.random() * 6) + 1;
@@ -7,25 +8,24 @@ const playerScore = (playerNumber) => {
 };
 
 const gameResult = { round: 0, player1: 0, player2: 0 };
-
-// ボタンを押した時に実行される関数
 const gameBtn = document.getElementById("game");
+const newRound = document.getElementById("round");
+const newPlayer1Count = document.getElementById("player1Count");
+const newPlayer2Count = document.getElementById("player2Count");
+// ボタンを押した時に実行される関数
 gameBtn.addEventListener("click", () => {
   if (gameResult.round >= 3) {
-    return;
+    return (disabled = true);
   }
-  const newRound = document.getElementById("round");
   const gamePoint1 = playerScore("player1");
   const gamePoint2 = playerScore("player2");
 
   if (gamePoint1 > gamePoint2) {
     // プレイヤー1のポイント数を変更
-    const newPlayer1Count = document.getElementById("player1Count");
     gameResult.player1++;
     newPlayer1Count.textContent = `プレイヤー1:${gameResult.player1}ポイント`;
   } else if (gamePoint1 < gamePoint2) {
     // プレイヤー2のポイント数を変更
-    const newPlayer2Count = document.getElementById("player2Count");
     gameResult.player2++;
     newPlayer2Count.textContent = `プレイヤー2:${gameResult.player2}ポイント`;
   }
