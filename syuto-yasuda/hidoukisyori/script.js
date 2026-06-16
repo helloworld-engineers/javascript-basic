0a85546ce98a2479db32bf6557e90eec7c402637
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-06-16 16:46:56 +0900

    ローディングのCSS表示をJS側で入れ替えに修正しました

--- syuto-yasuda/hidoukisyori/script.js
+++ syuto-yasuda/hidoukisyori/script.js
@@ -1,12 +1,10 @@
 const newBtn = document.getElementById("btn");
-const newLoading = document.getElementById("loading");
 const API_URL = "https://dog.ceo/api/breeds/image/random";
 const result = document.getElementById("dogEvent");
 
-// DogAPIから犬の画像を表示する関数
 async function getDogImage() {
   try {
-    newLoading.style.display = "block";
+    result.textContent = "ローディング中";
     const response = await fetch(API_URL);
     if (!response.ok) {
       throw new Error("Failed to fetch");
@@ -16,7 +14,6 @@ async function getDogImage() {
   } catch (error) {
     result.innerHTML = "<p>画像の取得に失敗しました</p>";
   } finally {
-    newLoading.style.display = "none";
   }
 }
 getDogImage();
