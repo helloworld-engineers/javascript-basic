ec6cd154fc9e1c0965ab9d5f808378a4e7cacd33
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-06-16 14:55:00 +0900

    非同期処理_yasuda

---
+++ syuto-yasuda/hidoukisyori/script.js
@@ -0,0 +1,28 @@
+// DogAPIから犬の画像を表示する関数
+async function getDate() {
+  const API_URL = "https://dog.ceo/api/breeds/image/random";
+  const newBtn = document.getElementById("btn");
+  const newLoading = document.getElementById("loading");
+  try {
+    newLoading.style.display = "flex";
+
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      console.log("error");
+    }
+    const date = await response.json();
+    const result = document.getElementById("dogEvent");
+    result.innerHTML = `<img src = ${date.message} class="dogSize" >`;
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    newLoading.style.display = "none";
+  }
+}
+getDate();
+
+// ボタンを押した時の処理
+const newBtn = document.getElementById("btn");
+newBtn.addEventListener("click", () => {
+  getDate();
+});
