657e54ff76e1eade559dd03b95525e4310437091
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-06-24 15:08:32 +0900

    JS総合課題_移動関数_yasuda

--- syuto-yasuda/JSsougoukadai/script.js
+++ syuto-yasuda/JSsougoukadai/script.js
@@ -1,3 +1,8 @@
+const newUpBtn = document.getElementById("upBtn");
+const newLeftBtn = document.getElementById("leftBtn");
+const newRightBtn = document.getElementById("rightBtn");
+const newDownBtn = document.getElementById("downBtn");
+
 let player = {
   HP: 100,
   attack: 10,
@@ -27,4 +32,57 @@ const masterMonsters = {
   },
 };
 
-let currentMonsters = randomMonster(MasterMonsters);
+// 移動キーを押した時にプレイヤーの位置を移動する関数
+const moveCharacter = (direction) => {
+  if (direction === "up") {
+    playerPosition.y += 1;
+  }
+  if (direction === "down") {
+    playerPosition.y -= 1;
+  }
+
+  if (direction === "left") {
+    playerPosition.x -= 1;
+  }
+
+  if (direction === "right") {
+    playerPosition.x += 1;
+  }
+  return playerPosition;
+};
+
+// 上ボタンを押した時の処理
+newUpBtn.addEventListener("click", () => {
+  if (playerPosition.y === 30) {
+    return;
+  }
+  const direction = "up";
+  moveCharacter(direction);
+});
+
+// 下ボタンを押した時の処理
+newDownBtn.addEventListener("click", () => {
+  if (playerPosition.y === -30) {
+    return;
+  }
+  const direction = "down";
+  moveCharacter(direction);
+});
+
+// 左ボタンを押した時の処理
+newLeftBtn.addEventListener("click", () => {
+  if (playerPosition.x === -30) {
+    return;
+  }
+  const direction = "left";
+  moveCharacter(direction);
+});
+
+// 右ボタンを押した時の処理
+newRightBtn.addEventListener("click", () => {
+  if (playerPosition.x === 30) {
+    return;
+  }
+  const direction = "right";
+  moveCharacter(direction);
+});
