bc4abbc400dedea7a4d394411043b043c2b9f370
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-06-24 13:19:26 +0900

    モンスターの画像を追加しました

---
+++ syuto-yasuda/JSsougoukadai/script.js
@@ -0,0 +1,30 @@
+let player = {
+  HP: 100,
+  attack: 10,
+  level: 1,
+};
+
+let playerPosition = { x: 0, y: 0 };
+
+const masterMonsters = {
+  slime: {
+    HP: 20,
+    attack: 10,
+    exp: 10,
+    appearanceRate: 65,
+  },
+  doragon: {
+    HP: 40,
+    attack: 20,
+    exp: 15,
+    appearanceRate: 25,
+  },
+  metalslime: {
+    HP: 20,
+    attack: 10,
+    exp: 30,
+    appearanceRate: 10,
+  },
+};
+
+let currentMonsters = randomMonster(MasterMonsters);
