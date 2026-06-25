f309f3073857333c04ca6309c3f649f82a8c4495
DaisukeSuzuki <daisuke.suzuki@helloworld.works>
2026-06-25 18:56:18 +0900

    feat:JS総合課題全体組み立てPR

--- daisuke.suzuki/saisyu/scriptSaisyu.js
+++ daisuke.suzuki/saisyu/scriptSaisyu.js
@@ -18,6 +18,7 @@ const MONSTERS = {
     attack: 10,
     exp: 10,
     encounterRate: 0.65,
+    image: "slime.png",
   },
   dragon: {
     id: "dragon",
@@ -26,6 +27,7 @@ const MONSTERS = {
     attack: 20,
     exp: 15,
     encounterRate: 0.25,
+    image: "dragon.webp",
   },
   metal_slime: {
     id: "metal_slime",
@@ -34,6 +36,7 @@ const MONSTERS = {
     attack: 10,
     exp: 30,
     encounterRate: 0.1,
+    image: "Metal_slime.webp",
   },
 };
 
@@ -61,7 +64,7 @@ const MAPS = {
   grass: {
     name: "草原",
     area: { minX: -30, maxX: 0, minY: -30, maxY: 0 },
-    encounterRate: 0.4,
+    encounterRate: 0.1,
   },
   volcano: {
     name: "火山",
@@ -104,7 +107,9 @@ let MODE = {
   MAP: "MAP",
   BATTLE: "BATTLE",
 };
+
 let currentMode = MODE.MAP;
+
 //画面遷移：画面遷移
 function changeMode(nextMode) {
   currentMode = nextMode;
@@ -120,9 +125,6 @@ function changeMode(nextMode) {
   }
 }
 
-changeMode(currentMode);
-console.log(currentMode);
-
 //ログ表示：ログ配列
 let logList = [];
 const LOG_TYPE = {
@@ -133,7 +135,7 @@ const LOG_TYPE = {
 
 //ログ表示：ログ追加
 function addLog(message, type = LOG_TYPE.INFO) {
-  logList.push({
+  logList.unshift({
     message,
     type,
   });
@@ -211,12 +213,26 @@ function mapCheck(playerStatus, MAPS) {
 //マップ機能：エリア遷移(座標変更前後のマップ情報比較)
 function changeAria(playerStatus, MAPS) {
   const nextMap = mapCheck(playerStatus, MAPS);
+  console.log(playerStatus.mapId);
   if (!nextMap) return false;
   if (nextMap.id !== playerStatus.mapId) {
     playerStatus.mapId = nextMap.id;
+    changeBackground(playerStatus.mapId + "-map");
     return true;
   }
   return false;
+}
+
+//マップ機能：エリア移動に伴う背景変更
+function changeBackground(mapType) {
+  const map = document.getElementById("map-screen");
+  map.classList.remove(
+    "grass-map",
+    "volucano-map",
+    "village-map",
+    "seashore-map",
+  );
+  map.classList.add(mapType);
 }
 
 //エンカウント機能：エンカウント判定
@@ -238,6 +254,19 @@ function appearMonsterCheck(MONSTERS) {
   }
 }
 
+//モンスター画像表示
+function renderMonster(enemy) {
+  const img = document.getElementById("enemy-img");
+  img.src = enemy.image;
+  img.alt = enemy.name;
+}
+
+//プレイヤーステータス更新
+function renderPlayerStatus(playerStatus) {
+  console.log("STSRTB", playerStatus.hp);
+  document.getElementById("player-hp").textContent = "HP:" + playerStatus.hp; //この部分でundefinedが出ます
+}
+
 //モンスター生成
 function createMonster(MONSTERS, monsterId) {
   const base = MONSTERS[monsterId];
@@ -248,6 +277,7 @@ function createMonster(MONSTERS, monsterId) {
     currentHp: base.hp,
     attack: base.attack,
     exp: base.exp,
+    image: base.image,
   };
 }
 
@@ -290,11 +320,14 @@ function levelUp(currentExp, gainedExp, currentLevel, LEVEL_TABLE) {
 
 //戦闘時：戦闘開始
 function startBtl(playerStatus, enemyStatus) {
+  renderMonster(enemyStatus);
+  renderPlayerStatus(playerStatus);
   return {
     playerHp: playerStatus.hp,
     enemyHp: enemyStatus.currentHp,
   };
 }
+
 //戦闘時：両者ステータス生成
 let battleState = startBtl(playerStatus, monsterStatus);
 
@@ -328,6 +361,7 @@ function applyExp(playerStatus, enemyStatus) {
 
 //戦闘時：プレイヤー攻撃時処理
 function inputAttack(state, playerAtk, enemyAtk) {
+  console.log(state.playerHp);
   //プレイヤー攻撃
   const damage = calcDamage(playerAtk);
   const playerResult = calcHp(state.enemyHp, damage);
@@ -344,6 +378,7 @@ function inputAttack(state, playerAtk, enemyAtk) {
   const enemyDamage = calcDamage(enemyAtk);
   const enemyResult = calcHp(state.playerHp, enemyDamage);
   state.playerHp = enemyResult.nextHp;
+  renderPlayerStatus(state.playerHp);
   //log
   addLog(
     `プレイヤーは${enemyDamage}ダメージをうけた！ 残りHP: ${state.playerHp}`,
@@ -384,3 +419,82 @@ function inputEscape(state, enemyAtk, escapeRate) {
   }
   return "continue";
 }
+
+//一連の処理
+//移動処理
+
+function handleMove(direction) {
+  //境界チェック
+  if (!borderCheck(direction, playerStatus)) {
+    addLog("この方向には進めません");
+    return;
+  }
+  //移動
+  move(direction, playerStatus);
+  addLog(`${direction}方向に進みました`);
+  console.log(playerStatus.position);
+  //エリア移動
+  const prevMapId = playerStatus.mapId;
+  if (changeAria(playerStatus, MAPS)) {
+    addLog(`エリア移動:${prevMapId}>${playerStatus.mapId}`, LOG_TYPE.SYSTEM);
+  }
+  //エンカウント判定
+  if (!encountCheck(playerStatus, MAPS)) return;
+  //モンスター判定
+  const monsterId = appearMonsterCheck(MONSTERS);
+  //モンスター生成
+  const enemy = createMonster(MONSTERS, monsterId);
+  //戦闘開始
+  battleState = startBtl(playerStatus, enemy);
+  battleState.enemy = enemy;
+  //モード変更
+  changeMode(MODE.BATTLE);
+  ///ログ
+  addLog(`${enemy.name}が現れた！`, LOG_TYPE.BATTLE);
+}
+
+//ボタンと接続、イベント追加
+document.querySelectorAll(".c-button").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const dir = e.target.id;
+    handleMove(dir);
+  });
+});
+
+//戦闘処理
+function handleBattleEnd(result) {
+  if (result === "continue") return;
+
+  if (result === "win") {
+    const expResult = applyExp(playerStatus, battleState.enemy);
+    addLog(`${expResult.gainExp}Exp獲得!`, LOG_TYPE.SYSTEM);
+
+    if (expResult.isLevelUp) {
+      addLog("レベルアップ！", LOG_TYPE.SYSTEM);
+    }
+  }
+  if (result === "lose") {
+    addLog("ゲームオーバー...", LOG_TYPE.SYSTEM);
+  }
+  if (result === "escape") {
+    addLog("逃走に成功した！", LOG_TYPE.SYSTEM);
+  }
+  changeMode(MODE.MAP);
+}
+
+//戦闘ボタン処理
+document.getElementById("btn-attack").addEventListener("click", () => {
+  const result = inputAttack(
+    battleState,
+    playerStatus.attack,
+    battleState.enemy.attack,
+  );
+
+  handleBattleEnd(result);
+});
+
+//逃走ボタン処理
+document.getElementById("btn-escape").addEventListener("click", () => {
+  const result = inputEscape(battleState, battleState.enemy.attack, escapeRate);
+  handleBattleEnd(result);
+});
