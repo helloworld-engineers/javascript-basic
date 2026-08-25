6f404953e28b7cf465c1eaa4d12882ab61358bc9
unknown <rintaro.chiba@helloworld.works>
2026-08-25 11:13:44 +0900

    Javascrip非同期処理①レビュー後修正①

--- rintaro.chiba/hidoukishori1/script.js
+++ rintaro.chiba/hidoukishori1/script.js
@@ -7,11 +7,8 @@ const loadingText = document.getElementById("loadingText");
 
 //asyncで関数を定義することでawait(同期処理と同じ上から順に処理)を使える
 //犬の画像を取得し代入する関数
-async function getDog() {
+const getDog = async () => {
   try {
-    loadingText.textContent = "読み込み中…";
-    loadingText.classList.remove("hide");
-
     // await: fetchが完了するまで一時停止,responseに
     const response = await fetch(API_URL);
     //HTTPステータスが200~299ではない場合
@@ -20,21 +17,29 @@ async function getDog() {
     }
     //promiseをjson形式で格納し、keyがmessageのvalueをimg要素のsrc属性に代入
     const data = await response.json();
-    img.src = data.message;
     return data;
   } catch (error) {
     //エラーが起こった際にエラーメッセージを表示
     console.error("非同期エラー:", error.message);
   } finally {
-    //最後に必ず行う処理,読み込み中の文字にdisplay:noneがついたclassを付与(＋0.3秒のラグ)
-    setTimeout(() => {
-      loadingText.classList.add("hide");
-    }, 300);
+    //読み込み中の文字にdisplay:noneがついたclassを付与
+
+    loadingText.classList.add("hide");
+    img.classList.remove("hide");
   }
-}
+};
 
 //画像取得をクリックした際にgetDog()を実行
-getBtn.addEventListener("click", getDog);
+getBtn.addEventListener("click", async () => {
+  img.classList.add("hide");
+  loadingText.classList.remove("hide");
+  loadingText.textContent = "読み込み中…";
+  const data = await getDog();
+  img.src = data.message;
+});
 
 //初期実行
-getDog();
+window.onload = async () => {
+  const data = await getDog();
+  img.src = data.message;
+};
