b6e8c7dda217cc5cd7c51acf8e34accd42208720
yuma kikuchi <yumakikuchi@MacBook-Air.local>
2026-09-26 18:10:22 +0900

    ローディングの修正

--- yuma.kikuchi/hidouki1/hidouki.js
+++ yuma.kikuchi/hidouki1/hidouki.js
@@ -1,18 +1,20 @@
 const API_URL = "https://dog.ceo/api/breeds/image/random";
 const img = document.getElementById("img");
 const imgBtn = document.getElementById("imgBtn");
+
 // 非同期処理
 const getImg = () => {
+  img.textContent = "ローディング中です！";
   fetch(API_URL)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       const dataMessage = data.message;
-      img.src = dataMessage;
+      img.innerHTML = `<img  class="dog" src = ${dataMessage} alt="犬の画像"/> `;
     })
     .catch((error) => {
-      console.error(`失敗です`, error);
+      alert(`失敗です`);
     });
 };
 getImg();
